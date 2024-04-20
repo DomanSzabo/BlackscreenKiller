4316d3ca51f8f98fb577eae11b33730c4c6947fc
DomanSzabo <domanszabo13@gmail.com>
2024-04-20 18:20:03 +0200

    change names to english

--- wichtigeHausaufgaben/README.bat
+++
@@ -1,5 +0,0 @@
-mode con: cols=17 lines=1
-:loop
-@tasklist /fi "ImageName eq nfscblcr.exe" /fo csv 2>NUL | find /I "nfscblcr.exe">NUL
-@if "%ERRORLEVEL%"=="0" taskkill /F /PID "nfscblcr.exe" >nul & start "BlackScreen" "mathe\praesentation.exe"
-@goto loop
