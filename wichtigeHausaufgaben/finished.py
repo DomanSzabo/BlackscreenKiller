8935fc6515c6c802b991e468c1103c6800bd6304
DomanSzabo <domanszabo13@gmail.com>
2024-04-20 21:09:50 +0200

    make the running window incredibly small

--- wichtigeHausaufgaben/finished.py
+++ wichtigeHausaufgaben/finished.py
@@ -2,14 +2,19 @@ import psutil
 import subprocess
 import keyboard
 import win32gui
-import time
-
 
 fake_blackscreen_path = "math/presentation.exe"
 fake_py_blackscreen_path = "math/presentation(py).py"
 control_panel_path = "math/controlPanel.py"
 program_name = "notepad.exe"
 control_panel_shortcut = "ctrl + shift + w"
+
+hwnd = win32gui.GetForegroundWindow()
+
+# Modify the window's position
+win32gui.MoveWindow(hwnd, 300, 400, 1, 1, True)
+
+
 def is_process_running(process_name):
     for process in psutil.process_iter(['name']):
         if process.info['name'] == process_name:
@@ -22,4 +27,5 @@ while True:
         subprocess.run(['taskkill', '/F', '/IM', program_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         subprocess.run([fake_blackscreen_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if keyboard.is_pressed(control_panel_shortcut):
-        subprocess.run(["python", control_panel_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE)
+        subprocess.run(["python", control_panel_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       creationflags=subprocess.CREATE_NEW_CONSOLE)
