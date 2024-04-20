2a419f3be43ef21c1f38cdbcf1ec96ed78ec26fe
DomanSzabo <domanszabo13@gmail.com>
2024-04-20 19:59:10 +0200

    make the program run control panel (not working properly (cant run blackscreen))

--- wichtigeHausaufgaben/finished.py
+++ wichtigeHausaufgaben/finished.py
@@ -1,9 +1,12 @@
 import psutil
 import subprocess
+import keyboard
 
 
-program_path = "math/presentation.exe"
+fake_blackscreen_path = "math/presentation.exe"
+control_panel_path = "math/controlPanel.py"
 program_name = "notepad.exe"
+control_panel_shortcut = "ctrl + shift + w"
 def is_process_running(process_name):
     for process in psutil.process_iter(['name']):
         if process.info['name'] == process_name:
@@ -14,4 +17,6 @@ def is_process_running(process_name):
 while True:
     if is_process_running(program_name):
         subprocess.run(['taskkill', '/F', '/IM', program_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        subprocess.run([program_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run([fake_blackscreen_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if keyboard.is_pressed(control_panel_shortcut):
+        subprocess.run(["python", control_panel_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_CONSOLE)
